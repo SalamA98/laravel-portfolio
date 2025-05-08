fbd0378c5626fb0fda2d1270a58b8c4a1f841248
Salam Arida <salam1998na@gmail.com>
2025-05-08 20:12:19 +0300

    define layouts

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- footer -->
+<div class="container">
+    <footer class="footer">
+        <p class="mb-0">Copyright
+            <script>document.write(new Date().getFullYear())</script> &copy; <a
+                href="http://www.devcrud.com">DevCRUD</a> Distribution <a
+                href="https://themewagon.com">ThemeWagon</a>
+        </p>
+        <div class="social-links text-right m-auto ml-sm-auto">
+            <a href="mailto:Salam1998na@gmail.com" class="link" target="_blank"><i class="ti-email"></i></a>
+            <a href="https://www.linkedin.com/in/salam-arida/" class="link" target="_blank"><i class="ti-linkedin"></i></a>
+            <a href="https://github.com/SalamA98" class="link" target="_blank"><i class="ti-github"></i></a>
+        </div>
+    </footer>
+</div> <!-- end of page footer -->
---
+++ resources/views/sections/services.blade.php
@@ -0,0 +1,54 @@
+<!-- service section -->
+<section class="section" id="service">
+    <div class="container text-center">
+        <p class="section-subtitle">What I Do ?</p>
+        <h6 class="section-title mb-6">Service</h6>
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="{{ asset('imgs/pencil-case.svg') }}"
+                            alt="Landing page icon" class="icon">
+                        <h6 class="title">Adipisicing</h6>
+                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam
+                            molestias dolorem iste quod.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="{{ asset('imgs/responsive.svg') }}"
+                            alt="Landing page icon" class="icon">
+                        <h6 class="title">Sapiente</h6>
+                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam
+                            molestias dolorem iste quod.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="{{ asset('imgs/toolbox.svg') }}"
+                            alt="Landing page icon" class="icon">
+                        <h6 class="title">Placeat</h6>
+                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam
+                            molestias dolorem iste quod.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="{{ asset('imgs/analytics.svg') }}"
+                            alt="Landing page icon" class="icon">
+                        <h6 class="title">Iusto</h6>
+                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam
+                            molestias dolorem iste quod.</p>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end of row -->
+    </div>
+</section><!-- end of service section -->
