be18cd2e2d4996ddff84396b228c0a7b42d7b8c3
Salam Arida <salam1998na@gmail.com>
2025-05-10 00:30:54 +0300

    Message Controller

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+
+    public function index(){
+        $messages = Message::all();
+        return view('messages.index', compact('messages'));
+    }
+
+    public function store(Request $request){
+        //$data = $request -> all();
+        //$request->dd();
+        $request-> validate([
+            'name' => 'required | min:3 | max: 50',
+            'email' => 'required | email',
+            'content' => 'required',
+        ]);
+        
+        $message = new Message();
+        $message-> name = $request->name;
+        $message-> email = $request->email;
+        $message-> content = $request->content;
+        $message->save();
+        return redirect('/#contact');
+        /*dd($request->all());
+        thank you page return view('sections\header');*/
+    }
+
+    public function show($id){
+        $message = Message::findOrFail($id);
+        return view('messages.show',compact('message'));
+    }
+}
