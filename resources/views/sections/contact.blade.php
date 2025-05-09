58e5325c325acd693db42b95182fe3fd29399db0
Salam Arida <salam1998na@gmail.com>
2025-05-09 23:01:59 +0300

    form validation

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -4,18 +4,42 @@
         <p class="section-subtitle">How can you communicate?</p>
         <h6 class="section-title mb-5">Contact Me</h6>
         <!-- contact form -->
+        <!--@if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+
+                    @foreach ($errors->all() as $error)
+
+                        <li>{{ $error }}</li>
+
+                    @endforeach
+
+                </ul>
+            </div>
+        @endif-->
+
         <form  action="/contact" method="POST" class="contact-form col-md-10 col-lg-8 m-auto">
             @csrf
             <div class="form-row">
                 <div class="form-group col-sm-6">
-                    <input type="text" name="name" size="50" class="form-control" placeholder="Your Name" required>
+                    <input type="text" name="name" size="50" class="form-control @error('name') isinvalid @enderror"  placeholder="Your Name" required value="{{ old('name') }}">
+                    @error('name')
+                        <div class="Invalid-feedback section-subtitle">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="form-group col-sm-6">
-                    <input type="email" name="email" class="form-control" placeholder="Enter Email" requried>
+                    <input type="email" name="email" class="form-control @error('email') isinvalid @enderror" placeholder="Enter Email" requried value="{{ old('email') }}">
+                    @error('email')
+                        <div class="Invalid-feedback section-subtitle">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group col-sm-12">
-                    <textarea name="content" id="comment" rows="6" class="form-control"
-                        placeholder="Write Something"></textarea>
+                    <textarea name="content" id="comment" rows="6" class="form-control @error('content') isinvalid @enderror"
+                        placeholder="Write Something" value= {{ old('content') }}></textarea>
+                    @error('content')
+                    <div class="Invalid-feedback section-subtitle">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group col-sm-12 mt-3">
                     <input type="submit" value="Send Message" class="btn btn-outline-primary rounded">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Message;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +21,34 @@ use Illuminate\Support\Facades\Route;
 });*/
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::view('/contact', 'pages.contact');
+Route::post('/contact',function(HttpRequest $request){
+    //$data = $request -> all();
+    //$request->dd();
+    $request-> validate([
+        'name' => 'required | min:3 | max: 50',
+        'email' => 'required | email',
+        'content' => 'required',
+    ]);
+    
+    $message = new Message();
+    $message-> name = $request->name;
+    $message-> email = $request->email;
+    $message-> content = $request->content;
+    $message->save();
+    return redirect('/#contact');
+    /*dd($request->all());
+    thank you page return view('sections\header');*/
+}); 
+
+Route::get('/admin/messages',function(){
+    $messages = Message::all();
+    return view('messages.index', compact('messages'));
+});
+
+Route::get('/admin/messages/{id}',function($id){
+    $message = Message::find($id);
+    return view('messages.show',compact('message'));
 });
