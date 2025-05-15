a31e2f4c8c153ec8c8f8ec11467101f3cf9d92f3
Salam Arida <salam1998na@gmail.com>
2025-05-15 23:02:59 +0300

    add volunteer section ad static section

---
+++ resources/views/sections/volunteering.blade.php
@@ -0,0 +1,29 @@
+<!-- volunteering section -->
+<section class="section" id="volunteering">
+    <div class="container text-center">
+        <p class="section-subtitle">Giving Back</p>
+        <h6 class="section-title mb-4">Volunteering</h6>
+        <!-- info card -->
+        <div  class="row justify-content-center ">
+            <div class="col-md-8 d-flex  testimonial-card-body ">
+                <!-- content -->
+                <div class="flex-grow-1">
+                    <h5 class="text-center mb-2">Volunteer at RBS (Red Blood Cells Team)</h5>
+                    <p class="text-center"><strong>Duration:</strong> Feb 2017 – Feb 2019</p>
+                    <p class="text-center"> <strong>Activities:</strong></p>
+                    <ul class="text-left">
+                        <li>Wrote, reviewed, and refined university lecture notes.</li>
+                        <li>Researched references and solved past exam questions for student support.</li>
+                        <li>Taught children Scratch programming during World Children’s Day.</li>
+                    </ul>
+                    <!-- download button -->
+                    <div class="text-center mt-4">
+                        <a href="{{ asset('docs/volunteer-certificate.pdf') }}" target="_blank" class="btn btn-outline-primary mt-3 ">
+                            📄 Download Certificate
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     @include('sections.header')
     @include('sections.about')
-    @include('sections.services')
+
     @include('sections.portfolio')
     @include('sections.hireMe')
-    @include('sections.testimonials')
-    @include('sections.blog')
+    @include('sections.volunteering')
+    {{--@include('sections.testomonials')--}}
+    {{--@include('sections.blog')--}}
+    @include('sections.services')
     @include('sections.contact')
 @endsection
